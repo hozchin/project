d1ad30abdc1baf814b3408907fdaf5a9273e7341
Artem <example@gmal.com>
2024-10-14 22:20:14 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+"use strict"
+
+// console.log('arr' + " - object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%1);
+
+// console.log(2 + 2 * 2 != 8);
+
+// const isChecked = true,
+//     isClosed = false;
+
+// console.log(isChecked || isClosed);
+
+// const category = 'toys';
+// console.log(`https://someur.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Hi, ${user}`);
+
+// alert('hello');
+
+// const answer = +prompt("How are u old?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('How ur name?', '');
+// answers[1] = prompt('How ur second name?', '');
+// answers[2] = prompt('How ur old?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// document.write(answers);
+
+// const result = confirm("Are u here?");
+// console.log(result);
+
+// let number=4;
+// const persone = "Alex";
+
+// const bool = false;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// // console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[2]);
